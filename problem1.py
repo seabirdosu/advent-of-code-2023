8c8a3cc1c4609f036110eb7572ca42ede1589277
seabirdosu <seabirdosu@gmail.com>
2024-08-28 22:21:14 +0200

    for laptop

---
+++ problem1.py
@@ -0,0 +1,15 @@
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+sum = 0
+
+for x in lista:
+    x = x**2
+for num in 
+
+ # sum is supposed to take n^2 + n2^2...   
+ # after 1 iteration sum is supposed to be n1**2
+ # after 2 iteration sum is supposed to be n1**2 + n2**2
+ # after 3 iteration sum is supposed to be n1**2 + n2**2 + n3**2
+#sum = 1**2 + 2**2
+#sum = 1**2 + 2**2 + 3**2 
+
